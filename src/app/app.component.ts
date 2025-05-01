45d6624ed626d3c916b9d250e8cdba40662dad92
fede1975 <federicorizzato75@gmail.com>
2025-05-01 18:53:28 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+//import { Observable } from 'rxjs';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+
+
+data:any;
+data2:any;
+
+  constructor(private http: HttpClient) {}
+
+
+
+
+/*private apiUrl ="https://api.openweathermap.org/data/2.5/weather?lat=27.9506&lon=-82.4572&appid=71c406cd0696048af1069afda7277927";
+*/
+
+
+
+
+    ngOnInit() {      
+        
+
+
+navigator.geolocation.getCurrentPosition(position => {
+
+        
+const lat = position.coords.latitude
+const lng = position.coords.longitude
+console.log(lat)
+
+        this.http.get<any>("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&appid=71c406cd0696048af1069afda7277927").subscribe(data => {
+            this.data = data.main.temp-273.17;
+            this.data2 = data.weather[0].icon
+               
+
+        })
+
+  });
+      
+
+
+  
+   
+
+ }
+
+
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
